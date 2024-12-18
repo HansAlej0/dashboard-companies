7372e350f895dcf8ced966ec869235a8aefa576c
Alej0C <hansalejandroca@hotmail.com>
2024-12-18 15:21:26 -0500

    Página FAQS completada

---
+++ app/(routes)/faqs/components/AccordionFaqs/AccordionFaqs.data.ts
@@ -0,0 +1,26 @@
+export const dataFaqs = [
+    {
+        id: 1,
+        question: "¿Qué es el dashboard de empresas?",
+        answer:
+            "Nuestro dashboard de empresas es una plataforma diseñada para facilitar la gestión y el análisis de datos empresariales en un solo lugar. Proporciona herramientas intuitivas y potentes para visualizar métricas clave, realizar seguimientos de desempeño, y generar reportes personalizados. Es ideal para empresas que buscan optimizar su toma de decisiones y mejorar su productividad.",
+    },
+    {
+        id: 2,
+        question: "¿Qué beneficios ofrece el dashboard para mi empresa?",
+        answer:
+            "El dashboard centraliza toda la información relevante de tu empresa, permitiéndote acceder a datos en tiempo real. Esto no solo ahorra tiempo, sino que también mejora la precisión en el análisis y la planeación estratégica. Además, su diseño adaptable asegura que puedas visualizar tus métricas desde cualquier dispositivo, sin complicaciones.",
+    },
+    {
+        id: 3,
+        question: "¿Es personalizable el dashboard?",
+        answer:
+            "¡Por supuesto! Entendemos que cada empresa tiene necesidades únicas. Nuestro dashboard es altamente personalizable, lo que te permite ajustar gráficos, informes y vistas según tus requerimientos. Desde pequeños negocios hasta grandes corporaciones, nuestra plataforma se adapta a tus objetivos específicos.",
+    },
+    {
+        id: 4,
+        question: "¿Cómo puedo acceder al dashboard de empresas?",
+        answer:
+            "Para acceder al dashboard de empresas, simplemente inicia sesión en tu cuenta y selecciona la opción 'Dashboard' en el menú principal. Desde allí, podrás explorar todas las funcionalidades y herramientas disponibles para optimizar la gestión de tu empresa.",
+    },
+];
---
+++ app/(routes)/faqs/page.tsx
@@ -0,0 +1,35 @@
+import { AccordionFaqs } from "./components/AccordionFaqs";
+
+export default function PageFaqs() {
+    return (
+        <div className="max-w-4xl p-6 mx-auto rounded-lg shadow-md bg-background">
+            <h2 className="mb-8 text-3xl">FAQS</h2>
+            <div className="mb-5">
+                <p>
+                    Bienvenido a nuestra sección de Preguntas Frecuentes (FAQ) diseñada
+                    específicamente para brindarte respuestas rápidas y claras sobre el
+                    dashboard para empresas que hemos desarrollado con pasión y
+                    dedicación. En nuestra página de FAQs, encontrarás una recopilación de
+                    las preguntas más comunes que nuestros usuarios suelen hacer sobre el
+                    funcionamiento, características y uso de nuestro dashboard.
+                </p>
+                <p>
+                    Desde cómo registrarte en la plataforma hasta cómo aprovechar al
+                    máximo sus funciones, hemos reunido una lista exhaustiva de
+                    interrogantes para ofrecerte la mejor experiencia posible. Nuestro
+                    equipo se ha esforzado por proporcionar respuestas detalladas y
+                    fáciles de entender para garantizar que encuentres la información que
+                    necesitas de manera rápida y sencilla.
+                </p>
+                <p>
+                    Si no encuentras la respuesta que buscas, no dudes en contactarnos.
+                    Estamos aquí para ayudarte en cada paso del camino. Explora nuestras
+                    FAQs y descubre cómo nuestro dashboard puede impulsar la eficiencia y
+                    el éxito de tu empresa.
+                </p>
+            </div>
+
+            <AccordionFaqs />
+        </div>
+    );
+}
